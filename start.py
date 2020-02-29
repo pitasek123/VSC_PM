71ed10e74af8104b37cf31f0027ea5ae843c507d
malyszka <pmalyszka@wp.pl>
2020-02-29 12:00:00 +0100

    Idę do domu

---
+++ start.py
@@ -0,0 +1,32 @@
+print("Hello world!")
+
+# def main():
+    # for elem in kolekcja:
+        # costam()
+    
+
+# łańcuchy znaków
+
+liczba = "Ala"
+liczba = 0
+liczba = 0.0
+# PEP8
+imie = "Ala"
+print(imie)
+print(type(5))
+print(type(imie))
+print(type(4.4))
+print(type(True))
+print(type(None))
+
+imie = str("Ala")
+liczba = str(100)
+
+binarnie = "101"
+print(int(binarnie,2))
+
+
+imie = imie.lower()
+# imie[0] = 'a' błąd
+print(imie)
+"Ala".lower().title().count("a")
